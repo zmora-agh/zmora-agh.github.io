837fcfd8f5fbc58e42d396e1298a870fffefb032
Memleak Teamcity <teamcity@maxmati.pl>
2017-03-18 17:43:30 +0100

    Develop deploy

--- 11.14b1850ea12f59f5b6f7.chunk.js
+++
@@ -1,1 +0,0 @@
-webpackJsonp([11],{"./app/containers/NewsPage/sagas.js":function(e,t,a){"use strict";function r(){return regeneratorRuntime.wrap(function(e){for(;;)switch(e.prev=e.next){case 0:case"end":return e.stop()}},n[0],this)}Object.defineProperty(t,"__esModule",{value:!0}),t.defaultSaga=r;var n=[r].map(regeneratorRuntime.mark);t.default=[r]}});
